949281bf1e884b66f20f2e5b097470ac13737de6
chandu7313 <mrcm7313@gmail.com>
2026-08-08 11:46:42 +0530

    feat(flutter/constants): add agricultural domain constants

---
+++ kissan_mithar/lib/core/constants/app_constants.dart
@@ -0,0 +1,14 @@
+class AppConstants {
+  AppConstants._();
+
+  static const String baseUrl = 'https://api.kissanmithar.in/v1';
+  static const int connectTimeoutMs = 15000;
+  static const int receiveTimeoutMs = 15000;
+
+  // Measurement conversions
+  static const int guntasPerAcre = 40;
+  static const int centsPerAcre = 100;
+
+  // Min touch target
+  static const double minTouchTarget = 56.0;
+}
